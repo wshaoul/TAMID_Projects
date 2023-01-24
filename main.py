7b87423d1ae489ad5cdbf766414565e8634727e3
wshaoul <82252983+wshaoul@users.noreply.github.com>
2023-01-24 10:49:30 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+def isPalindrome(word) -> bool:
+    return word == ''.join(list(reversed(word)))
+
+def sortArrays(arr1, arr2, k):
+    arr1.extend(arr2)
+    return sorted(arr1)[:k]
+
+
+
+
+def main():
+    print(isPalindrome("racecar"))
+    print(isPalindrome("poop"))
+    print(isPalindrome("hub"))
+    print(sortArrays([1, 3, 5, 8], [4], 10))
+
+
+if __name__ == '__main__':
+    main()
+
